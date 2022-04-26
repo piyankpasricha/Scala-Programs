32300b32646ff1bf71922cd6d351c4f2a56b5d25
piyankpasricha <104265876+piyankpasricha@users.noreply.github.com>
2022-04-26 22:55:19 +0530

    Update Reverselist.scala

--- Reverselist.scala
+++ Reverselist.scala
@@ -1,25 +1,9 @@
+//Run on hackerrank editor to see output
 import scala.collection.mutable.ListBuffer
-object Reverselist {
-  def main(args: Array[String]): Unit ={
-    println("Enter the number of element in List:")
-    var num: Int =scala.io.StdIn.readInt()
-    val mList1: ListBuffer[Int] = ListBuffer()
-    for (j <-0 until num) {
-      mList1.addOne(scala.io.StdIn.readInt())
-    }
-    println("Enter the orignal List:")
-    println(mList1)
-    println("Enter the Reversed List:")
-    println(reverse(mList1))
-
-  }
-  def reverse(l:ListBuffer[Int]):ListBuffer[Int]={
+def f(arr:List[Int]):List[Int] ={
     val mList2: ListBuffer[Int] = ListBuffer()
-    for (i <- l.size - 1 to 0 by -1) {
-      mList2.addOne(l(i))
+    for (i <- arr.size - 1 to 0 by -1) {
+      mList2.addOne(arr(i))
     }
-    return mList2
-  }
-
-
+    return mList2.toList
 }
