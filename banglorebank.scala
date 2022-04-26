dd2e2a950234285494fb0d3e0fea8cd8ef7cb238
piyankpasricha <104265876+piyankpasricha@users.noreply.github.com>
2022-04-26 22:58:15 +0530

    Update banglorebank.scala

--- banglorebank.scala
+++ banglorebank.scala
@@ -1,10 +1,28 @@
+//Run on hackerrank editor to see output
 import scala.io.StdIn
-object banglorebank {
-  def solve(arr:Seq[Int],pre:Array[Array[Int]]= Array.ofDim[Int](10,10)):Int = {
+object Solution {
+
+    def main(args: Array[String]) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+        
+*/
+   val n = StdIn.readInt()
+    val arr:Vector[Int] = StdIn.readLine().split(' ').map(_.toInt).toVector
+    val pre:Array[Array[Int]]= Array.ofDim[Int](10,10)
+    println(solve(arr,pre))
+
+    }
+    
+    def solve(arr:Seq[Int],pre:Array[Array[Int]]):Int = {
+    
     def dist(a:Int, b:Int)= Math.abs(((a+9)%10)-((b+9)%10))
+    
     arr match {
+      
       case Nil => pre.foldLeft(Int.MaxValue)(_ min _.min)
+      
       case x+:xs =>
+      
         val now = Array.tabulate[Int](10,10) {
           case (`x`,j) => (for(k <- 0 until 10) yield pre(k)(j)+dist(x,k)+1).min
           case (i,`x`) => (for(k <- 0 until 10) yield pre(i)(k)+dist(x,k)+1).min
@@ -13,12 +31,4 @@ object banglorebank {
         solve(xs,now)
     }
   }
-
-
-  def main(args: Array[String]) {
-    val n = StdIn.readInt()
-    val arr:Vector[Int] = StdIn.readLine().split(' ').map(_.toInt).toVector
-    println(solve(arr))
-  }
-
 }
