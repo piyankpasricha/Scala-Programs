51da87a3366f804201e17506c7e0ec22706be57a
piyankpasricha <104265876+piyankpasricha@users.noreply.github.com>
2022-04-26 23:01:45 +0530

    Update captainprime.scala

--- captainprime.scala
+++ captainprime.scala
@@ -1,12 +1,18 @@
+
+//Run on hackerrank editor to see output
 import scala.io.StdIn.readLine
-object captainprime {
-  def main(args: Array[String]) {
-    val t = scala.io.StdIn.readInt
+
+object Solution {
+
+    def main(args: Array[String]) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+        
+*/
+ val t = scala.io.StdIn.readInt
     for(i <- 1 to t)
       println(decide(readLine));
-  }
-
-  def decide(s:String):String = {
+    }
+def decide(s:String):String = {
     if(s.contains("0") || !BigInt(s).isProbablePrime(20)) return "DEAD";
     else{
       val R = isRight(s.dropRight(1));
@@ -29,6 +35,4 @@ object captainprime {
     if(s.isEmpty) return true;
     return BigInt(s).isProbablePrime(20) && isRight(s.dropRight(1));
   }
-
-
 }
