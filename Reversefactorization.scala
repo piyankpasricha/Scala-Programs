5611a666759efb6697c5b005c5fb6970d6bb5cca
piyankpasricha <104265876+piyankpasricha@users.noreply.github.com>
2022-04-26 22:50:26 +0530

    Update Reversefactorization.scala

--- Reversefactorization.scala
+++ Reversefactorization.scala
@@ -1,21 +1,26 @@
-import scala.io.StdIn.readLine
-object Reversefactorization {
-  def main(args: Array[String]) {
+//Run on hackerrank editor to see output
+object Solution {
+
+    def main(args: Array[String]) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+   //taking inputs from user 
     val input = io.Source.stdin.getLines
     val pair = input.next.split(' ').map(_.toInt)
     val n = pair(0)
     val k = pair(1)
     val numbers = input.next.split(' ').map(_.toInt)
-
+   //checking number is divisor of given number or not
     val divisors = numbers.filter(x =>n % x == 0).sorted.reverse
 
     if (divisors.isEmpty) println(-1)
     else {
       println(findPath(n, divisors))
     }
-  }
-
-  def findPath(n: Int, divisors: Array[Int]): String = {
+    }
+ //Finding smallest path using nested function
+    def findPath(n: Int, divisors: Array[Int]): String = {
+      
     def checkPath(n: Int, divisors: Array[Int]): List[Int] = {
       if (divisors.isEmpty) throw new Exception("no solution")
       val divisor = divisors.head
@@ -35,5 +40,4 @@ object Reversefactorization {
       case e: Exception => "-1"
     }
   }
-
 }
