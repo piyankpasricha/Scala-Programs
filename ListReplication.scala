b136626f7cb1d6e484a23a57f9dabccc1227fe93
piyankpasricha <104265876+piyankpasricha@users.noreply.github.com>
2022-04-26 22:46:52 +0530

    Update ListReplication.scala

--- ListReplication.scala
+++ ListReplication.scala
@@ -1,25 +1,16 @@
+//Copy this code on hackerrank editor to see output
 import scala.collection.mutable.ArrayBuffer
-object ListReplication {
-  def main(args: Array[String]) {
-    println("Enter the number of times element should be repeated:")
-    var n: Int = scala.io.StdIn.readInt()
-    println("Enter the number of element in array")
-    var num: Int = scala.io.StdIn.readInt()
-    var elements = ArrayBuffer[Int]()
-    var element = ArrayBuffer[Int]()
-    for (j <-0 until num) {
-      var input = scala.io.StdIn.readInt()
-      elements.append(input)
-      for(k<-0 until n)
+var elements = ArrayBuffer[Int]()
+var element = ArrayBuffer[Int]() 
+//Function to print given number n number time in array
+def f(num:Int,arr:List[Int]):List[Int] ={
+      for (j <-0 until arr.length) {
+      elements.append(arr(j))
+      //Stoting n number of time in array each element
+      for(k<-0 until num)
       {
         element.append(elements(j))
       }
     }
-    for(j<-0 to element.length-1)
-    {
-      println(element(j))
-    }
-
-    }
-
-}
+     return element.toList
+ }
